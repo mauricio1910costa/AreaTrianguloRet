14d21fdc40ce387e2000e617f4067fca8afe4239
Mauricio Costa <mauriciof1910@hotmail.com>
2020-09-26 01:13:21 -0300

    resoluçao do exercicio

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AreaTrianguloRet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("A área é dada pela metade do produto da base pela altura.");
+            double b = 3;
+            double h = 5;
+            double a = 2;
+            double area = b * h / a;
+            Console.WriteLine($"{b} * {h} / {a} = {area} ");
+            
+
+
+            
+        }
+    }
+}
